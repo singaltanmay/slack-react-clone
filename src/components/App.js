dd39090a202c22d37fe89739571db8de768d2be1
singaltanmay <tanmaysingal@icloud.com>
2020-09-21 01:02:31 +0530

    Added routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { SignIn } from ".";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <SignIn />
+      <Switch>
+        <Route exact path="/" component={SignIn} />
+      </Switch>
     </div>
   );
 }
