9720d58f5ec92deac06720f52ffd054d8797a52b
singaltanmay <tanmaysingal@icloud.com>
2020-09-21 01:21:29 +0530

    Added some borders alignment etc

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 
 export default class Sidebar extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div id="sidebar">
+        <div className="user-profile">
+          <div>
+            <img className="user-avatar" />
+          </div>
+          <div>Tanmay Singal</div>
+        </div>
+        <div className="channels"></div>
+      </div>
+    );
   }
 }
